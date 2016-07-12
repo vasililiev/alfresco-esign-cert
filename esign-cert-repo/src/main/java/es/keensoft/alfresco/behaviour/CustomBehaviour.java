1755f48dd866e1b9e0f8aa353d32d2c4ae118a4e
Angel Borroy <angel.borroy@keensoft.es>
2016-07-12 16:14:41 +0200

    #5 Delete signatures (PDF)

--- esign-cert-repo/src/main/java/es/keensoft/alfresco/behaviour/CustomBehaviour.java
+++ esign-cert-repo/src/main/java/es/keensoft/alfresco/behaviour/CustomBehaviour.java
@@ -1,15 +1,10 @@
 package es.keensoft.alfresco.behaviour;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,8 +23,6 @@ import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.version.VersionService;
 import org.alfresco.service.namespace.QName;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 import com.itextpdf.text.pdf.AcroFields;
 import com.itextpdf.text.pdf.PdfPKCS7;
@@ -37,7 +30,9 @@ import com.itextpdf.text.pdf.PdfReader;
 
 import es.keensoft.alfresco.model.SignModel;
 
-public class CustomBehaviour implements NodeServicePolicies.OnDeleteAssociationPolicy, NodeServicePolicies.OnCreateNodePolicy {
+public class CustomBehaviour implements 
+    NodeServicePolicies.OnDeleteAssociationPolicy, 
+    NodeServicePolicies.OnCreateNodePolicy {
 	
 	private PolicyComponent policyComponent;
 	private NodeService nodeService;
@@ -45,13 +40,11 @@ public class CustomBehaviour implements NodeServicePolicies.OnDeleteAssociationP
 	private ContentService contentService;
 	
 	private static final String PADES = "PAdES";
-	private static Log log = LogFactory.getLog(CustomBehaviour.class);
 	
 	public void init() {
 		policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnDeleteAssociationPolicy.QNAME, 
 				SignModel.ASPECT_SIGNATURE, new JavaBehaviour(this, "onDeleteAssociation", 
 				NotificationFrequency.TRANSACTION_COMMIT));
-		log.debug("Enter create node");
 		policyComponent.bindClassBehaviour(
 		        NodeServicePolicies.OnCreateNodePolicy.QNAME,
 		        ContentModel.PROP_CONTENT,
@@ -63,23 +56,19 @@ public class CustomBehaviour implements NodeServicePolicies.OnDeleteAssociationP
 	@Override
 	public void onCreateNode(ChildAssociationRef childNodeRef) {
 
-		log.debug("Enter create node");
 		NodeRef node = childNodeRef.getChildRef();
-		ContentData contentData = (ContentData) nodeService.getProperty(node, ContentModel.PROP_CONTENT);
-		//Do this check only if the uploaded document is a PDF
-		if(contentData.getMimetype().equalsIgnoreCase("application/pdf")) {
-
-			log.debug("Is PDF");
-			try {
+		if (nodeService.exists(node)) {
+			ContentData contentData = (ContentData) nodeService.getProperty(node, ContentModel.PROP_CONTENT);
+			// Do this check only if the uploaded document is a PDF
+			if (contentData.getMimetype().equalsIgnoreCase("application/pdf")) {
 				ArrayList<Map<QName, Serializable>> signatures = getDigitalSignatures(node);
-				//Add the aspect asociation
 				if(signatures != null) {
 					for(Map<QName, Serializable> aspectProperties : signatures) {
 						String originalFileName = nodeService.getProperty(node, ContentModel.PROP_NAME).toString();
 						String signatureFileName = FilenameUtils.getBaseName(originalFileName) + "-" 
 						+ System.currentTimeMillis() + "-" + PADES;
 					
-						// Creating a node reference without type (no content and no folder), remains invisible for Share
+						// Creating a node reference without type (no content and no folder): remains invisible for Share
 						NodeRef signatureNodeRef = nodeService.createNode(
 								nodeService.getPrimaryParent(node).getParentRef(),
 								ContentModel.ASSOC_CONTAINS, 
@@ -94,61 +83,50 @@ public class CustomBehaviour implements NodeServicePolicies.OnDeleteAssociationP
 					}
 				}
 			}
-			catch(Exception ex) {
-				log.error(ex.toString());
-			}
 		}
 	}
 
 	@Override
 	public void onDeleteAssociation(AssociationRef nodeAssocRef) {
-		// Delete SIGNED aspect on SIGNATURE deletion
 		if (nodeService.exists(nodeAssocRef.getTargetRef())) {
 		    nodeService.removeAspect(nodeAssocRef.getTargetRef(), SignModel.ASPECT_SIGNED);
 		}
 	}
 	
-	public ArrayList<Map<QName, Serializable>> getDigitalSignatures(NodeRef node) throws IOException, KeyStoreException, Exception, CertificateException {
+	public ArrayList<Map<QName, Serializable>> getDigitalSignatures(NodeRef node) {
 		
-		ContentReader contentReader = contentService.getReader(node, ContentModel.PROP_CONTENT);
-		InputStream is = contentReader.getContentInputStream();
+		try {
 		
-		PdfReader reader = new PdfReader(is);
-        AcroFields af = reader.getAcroFields();
-        ArrayList<String> names = af.getSignatureNames();
-        if(names == null || names.isEmpty()) return null;
-        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-        ks.load(null, null);
-        ArrayList<Map<QName, Serializable>> aspects = new ArrayList<Map<QName, Serializable>>();
-        for (String name : names) {
-            PdfPKCS7 pk = af.verifySignature(name);
-            X509Certificate certificate = pk.getSigningCertificate();
-           
-            //Set aspect properties for each signature
-            Map<QName, Serializable> aspectSignatureProperties = new HashMap<QName, Serializable>(); 
-            aspectSignatureProperties.put(SignModel.PROP_DATE, convertCalendarToDate(pk.getSignDate()));
-    		aspectSignatureProperties.put(SignModel.PROP_CERTIFICATE_PRINCIPAL, certificate.getSubjectX500Principal().toString());
-    	    aspectSignatureProperties.put(SignModel.PROP_CERTIFICATE_SERIAL_NUMBER, certificate.getSerialNumber().toString());
-    	    aspectSignatureProperties.put(SignModel.PROP_CERTIFICATE_NOT_AFTER, certificate.getNotAfter());
-    	    aspectSignatureProperties.put(SignModel.PROP_CERTIFICATE_ISSUER, certificate.getIssuerX500Principal().toString());   
-    	    aspects.add(aspectSignatureProperties);
-        }
-		return aspects;
+			ContentReader contentReader = contentService.getReader(node, ContentModel.PROP_CONTENT);
+			InputStream is = contentReader.getContentInputStream();
+			
+			PdfReader reader = new PdfReader(is);
+	        AcroFields af = reader.getAcroFields();
+	        ArrayList<String> names = af.getSignatureNames();
+	        if(names == null || names.isEmpty()) return null;
+	        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+	        ks.load(null, null);
+	        ArrayList<Map<QName, Serializable>> aspects = new ArrayList<Map<QName, Serializable>>();
+	        for (String name : names) {
+	            PdfPKCS7 pk = af.verifySignature(name);
+	            X509Certificate certificate = pk.getSigningCertificate();
+	           
+	            //Set aspect properties for each signature
+	            Map<QName, Serializable> aspectSignatureProperties = new HashMap<QName, Serializable>(); 
+	            aspectSignatureProperties.put(SignModel.PROP_DATE, pk.getSignDate().getTime());
+	    		aspectSignatureProperties.put(SignModel.PROP_CERTIFICATE_PRINCIPAL, certificate.getSubjectX500Principal().toString());
+	    	    aspectSignatureProperties.put(SignModel.PROP_CERTIFICATE_SERIAL_NUMBER, certificate.getSerialNumber().toString());
+	    	    aspectSignatureProperties.put(SignModel.PROP_CERTIFICATE_NOT_AFTER, certificate.getNotAfter());
+	    	    aspectSignatureProperties.put(SignModel.PROP_CERTIFICATE_ISSUER, certificate.getIssuerX500Principal().toString());   
+	    	    aspects.add(aspectSignatureProperties);
+	        }
+			return aspects;
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
-	
-	@SuppressWarnings({"deprecation" })
-	private Date convertCalendarToDate(Calendar cal) {
-		Date date = new Date();
-		date.setDate(cal.get(Calendar.DATE));
-		date.setMonth(cal.get(Calendar.MONTH));
-		date.setYear(cal.get(Calendar.YEAR) - 1900);
-		date.setHours(cal.get(Calendar.HOUR));
-		date.setMinutes(cal.get(Calendar.MINUTE));
-		date.setSeconds(cal.get(Calendar.SECOND));
-		System.out.println(date);
-		return date;
-	}
 	
 	public PolicyComponent getPolicyComponent() {
 		return policyComponent;
@@ -181,4 +159,6 @@ public class CustomBehaviour implements NodeServicePolicies.OnDeleteAssociationP
 	public void setContentService(ContentService contentService) {
 		this.contentService = contentService;
 	}
+
+
 }
---
+++ esign-cert-repo/src/main/java/es/keensoft/alfresco/behaviour/TransactionalDeleteBehaviour.java
@@ -0,0 +1,161 @@
+package es.keensoft.alfresco.behaviour;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.policy.Behaviour;
+import org.alfresco.repo.policy.JavaBehaviour;
+import org.alfresco.repo.policy.PolicyComponent;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork;
+import org.alfresco.repo.transaction.AlfrescoTransactionSupport;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
+import org.alfresco.repo.transaction.RetryingTransactionHelper.RetryingTransactionCallback;
+import org.alfresco.repo.transaction.TransactionListener;
+import org.alfresco.service.cmr.repository.AssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.transaction.TransactionService;
+import org.alfresco.util.transaction.TransactionListenerAdapter;
+
+import es.keensoft.alfresco.model.SignModel;
+
+public class TransactionalDeleteBehaviour implements NodeServicePolicies.BeforeDeleteNodePolicy {
+ 
+	// Key to identify resources associated to transaction
+	private static final String KEY_RELATED_NODES = TransactionalDeleteBehaviour.class.getName() + ".relatedNodes";
+	 
+	private PolicyComponent policyComponent;
+	private NodeService nodeService;
+	private TransactionService transactionService;
+	private TransactionListener transactionListener;
+	private ThreadPoolExecutor threadPoolExecutor;
+	 
+	// Bind behaviour and initialize transaction listener
+	public void init() {
+	     
+	    policyComponent.bindClassBehaviour(
+	            NodeServicePolicies.BeforeDeleteNodePolicy.QNAME, 
+	            SignModel.ASPECT_SIGNED, 
+	            new JavaBehaviour(this, "beforeDeleteNode", Behaviour.NotificationFrequency.FIRST_EVENT));
+	     
+	    this.transactionListener = new RelatedNodesTransactionListener();
+	     
+	}
+	 
+	
+	@Override
+	public void beforeDeleteNode(NodeRef nodeRef) {
+	     
+	    // Bind listener to current transaction
+	    AlfrescoTransactionSupport.bindListener(transactionListener);
+	
+	    // Get some related nodes to work with
+	    List<NodeRef> relatedNodes = new ArrayList<NodeRef>();
+	    for (AssociationRef signatureAssoc : nodeService.getTargetAssocs(nodeRef, SignModel.ASSOC_SIGNATURE)) {
+	        relatedNodes.add(signatureAssoc.getTargetRef());
+	    }
+	
+	    // Transactions involving several nodes need resource updating
+	    List<NodeRef> currentRelatedNodes = AlfrescoTransactionSupport.getResource(KEY_RELATED_NODES);
+	    if (currentRelatedNodes == null) {
+	        currentRelatedNodes = relatedNodes;
+	    } else {
+	        currentRelatedNodes.addAll(relatedNodes);
+	    }
+	     
+	    // Put resources to be used in transaction listener
+	    AlfrescoTransactionSupport.bindResource(KEY_RELATED_NODES, currentRelatedNodes);
+	
+	}
+	 
+	// Listening "afterCommit" transaction event
+	private class RelatedNodesTransactionListener 
+	    extends TransactionListenerAdapter implements TransactionListener {
+	
+	    @Override
+	    public void afterCommit() {
+	        @SuppressWarnings("unchecked")
+	        List<NodeRef> nodesToBeReviewed = 
+	            (List<NodeRef>) AlfrescoTransactionSupport.getResource(KEY_RELATED_NODES);
+	        if (nodesToBeReviewed != null) {
+	            for (NodeRef nodeToBeReviewed : nodesToBeReviewed) {
+	                // Launch every node work in a different thread
+	                Runnable runnable = new RelatedNodeDeletion(nodeToBeReviewed);
+	                threadPoolExecutor.execute(runnable);
+	            }
+	        }
+	    }
+	     
+	    @Override
+	    public void flush() {
+	    }
+	     
+	}
+	 
+	// Thread to work with an individual node
+	private class RelatedNodeDeletion implements Runnable {
+	     
+	    private NodeRef nodeToBeReviewed;
+	     
+	    private RelatedNodeDeletion(NodeRef nodeToBeReviewed) {
+	        this.nodeToBeReviewed = nodeToBeReviewed;
+	    }
+	
+	    @Override
+	    public void run() {
+	        AuthenticationUtil.runAsSystem(new RunAsWork<Void>() {
+	             
+	            public Void doWork() throws Exception {
+	                 
+	                RetryingTransactionCallback<Void> callback = new RetryingTransactionCallback<Void>() {
+	                     
+	                    @Override
+	                    public Void execute() throws Throwable {
+	                        nodeService.deleteNode(nodeToBeReviewed);
+	                        return null;
+	                    }
+	                };
+	                 
+	                try {
+	                    RetryingTransactionHelper txnHelper = 
+	                        transactionService.getRetryingTransactionHelper();
+	                    txnHelper.doInTransaction(callback, false, true);
+	                } catch (Throwable e) {
+	                    e.printStackTrace();
+	                }
+	                 
+	                return null;
+	                 
+	            }
+	        });
+	    }
+	     
+	}
+
+	public void setPolicyComponent(PolicyComponent policyComponent) {
+		this.policyComponent = policyComponent;
+	}
+
+
+	public void setNodeService(NodeService nodeService) {
+		this.nodeService = nodeService;
+	}
+
+
+	public void setTransactionService(TransactionService transactionService) {
+		this.transactionService = transactionService;
+	}
+
+
+	public void setTransactionListener(TransactionListener transactionListener) {
+		this.transactionListener = transactionListener;
+	}
+
+
+	public void setThreadPoolExecutor(ThreadPoolExecutor threadPoolExecutor) {
+		this.threadPoolExecutor = threadPoolExecutor;
+	}
+}
